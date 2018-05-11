27019f706e200692c9db9acd33acfc4cff876265
Kristiyan Serafimov <kristian.plamenov@gmail.com>
2018-05-11 17:44:34 +0300

    feat: new features

--- src/db/model.js
+++ src/db/model.js
@@ -8,11 +8,11 @@ import Joi from 'joi';
 export const ClassModel = Joi.object({
   graduationYear: Joi.number(),
   members: Joi.array().items(Joi.string()),
-  hashtag_id: Joi.string()
+  hashtagId: Joi.string()
 }).required();
 
 export const ClassModelRequired = Joi.object({
   graduationYear: Joi.number().required(),
   members: Joi.array().items(Joi.string()).default([]),
-  hashtag_id: Joi.string()
+  hashtagId: Joi.string()
 }).required();
